4fb63e3065f033dede084e42e2248961a6d0f7a0
Boogyma <30368288+Boogyma@users.noreply.github.com>
2017-07-22 16:46:14 +0530

    Update and rename .gitignore to Posneg.c

---
+++ .gitignore/Posneg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+  int num;
+  printf("ENTER A NUMBER");
+  scanf("%d",&num);
+  if(num>0)
+   {
+     printf("NUMBER IS POSITIVE");
+   }
+   elseif(num<0)
+   {
+    printf("NUMBER IS NEGATIVE");
+   }
+   else
+   {
+    printf("NUMBER IS ZERO");
+    }
+}
+  
