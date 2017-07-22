031c81794b9135bd30addc78f1f0cb2966e33e83
Boogyma <30368288+Boogyma@users.noreply.github.com>
2017-07-22 16:54:53 +0530

    Create oddevn.c

---
+++ .gitignore/oddevn.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int num;
+  printf("Enter a number");
+  scanf("%d",&num);
+  if(num%2==0)
+  {
+    printf("Number is EVEN" );
+   }
+   else
+   {
+   printf("Number is ODD");
+   }
+  }
