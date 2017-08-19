ffd2a9012c41bd7e35cdf1d6968a3a2e5b2c607d
Boogyma <30368288+Boogyma@users.noreply.github.com>
2017-08-19 16:51:36 +0530

    num.c

---
+++ add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  int num a;
+  printf("Enter a number");
+  scanf("%d"&a);
+  if(a>0)
+  {
+    printf("Number is positive");
+    }
+    elseif{
+    printf("NUmber is negative");
+    }
+    else
+    {
+    printf("Number is Zero");
+    }
+ }
